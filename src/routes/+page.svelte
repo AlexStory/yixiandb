54949dd980d04c81c87148a3692242c00afab69c
Alex Story <astory11@yahoo.com>
2023-02-01 15:02:12 -0600

    added link to sim

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,7 +14,7 @@
     </div>
 </section>
 
-<section class="hero is-dark is-bold">
+<section class="hero is-dark is-bold mb-2">
     <div class="hero-body">
         <div class="columns">
             <div class="column is-two-thirds">
@@ -23,6 +23,21 @@
             <div class="column is-one-third">
                 <a href="/cards"><h1 class="title is-underlined">Cards</h1></a>
                 <h3 class="subtitle">learn about the cards of Yi Xian</h3>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="hero is-dark is-bold">
+    <div class="hero-body">
+        <div class="columns">
+            <div class="column is-one-third">
+                <a href="https://genkistudygames.com/yi-xian-damage-calculator/"><h1 class="title is-underlined">Simulator</h1></a>
+                <h3 class="subtitle">Check out Wereshrew's damage simulator</h3>
+            </div>
+            <div class="column is-two-thirds">
+                <img src="/images/sim.png" alt="damage simulator"/>
             </div>
         </div>
     </div>
